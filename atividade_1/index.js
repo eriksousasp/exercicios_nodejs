b1f13a02838daaa1d22fee34dbac4cba2c1c323a
eriksousasp <erik@nossocrm.com.br>
2020-06-20 18:19:55 -0300

    exercicio concluido

--- atividade_1/index.js
+++ atividade_1/index.js
@@ -1,5 +1,11 @@
-// ## Importe a biblioteca ##
+var utf8 = require('utf8');
+var readlineSync = require('readline-sync');
 
+// Wait for user's response.
+var numeroVoo = readlineSync.question('Qual o numero do seu voo? ');
 
-
-// ## Faça o código ##
+// Handle the secret text (e.g. password).
+var fileiraSentar = readlineSync.question('Qual a fileira deseja se sentar? ', {
+    hideEchoBack: true
+});
+console.log('Seu vôo de número ' + numeroVoo + ' está reservado na fileira ' + fileiraSentar + ' Obrigado! ');
