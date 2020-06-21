2efcae7be8ce07669b7c7ff2df214f8c28ef05b3
eriksousasp <erik@nossocrm.com.br>
2020-06-21 17:34:14 -0300

    atividades finalizadas

--- atividade_1/index.js
+++ atividade_1/index.js
@@ -1,9 +1,6 @@
-var utf8 = require('utf8');
 var readlineSync = require('readline-sync');
 
-// Wait for user's response.
 var numeroVoo = readlineSync.question('Qual o numero do seu voo? ');
 
-// Handle the secret text (e.g. password).
 var fileiraSentar = readlineSync.question('Qual a fileira deseja se sentar? ');
 console.log('Seu vôo de número ' + numeroVoo + ' está reservado na fileira ' + fileiraSentar + ' Obrigado! ');
